0e6727e2e384000a385e25d55e6d451366c3553d
Xenko10 <kubinios_2002@o2.pl>
2024-02-13 15:57:22 +0100

    make footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Hero from "./components/Hero/Hero.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.websitesWrapper}>
+        <a href='https://github.com/Xenko10'>
+          <img src='./src/assets/icons/github.png' alt='github' />
+        </a>
+        <a href='https://www.linkedin.com/in/jakub-ko%C5%82aczy%C5%84ski-6027b4293//'>
+          <img src='./src/assets/icons/linkedin.png' alt='linkedin' />
+        </a>
+        <a href='mailto:jakubkolaczynski0@gmail.com'>
+          <img src='./src/assets/icons/gmail.png' alt='gmail' />
+        </a>
+      </div>
+      <hr />© {new Date().getFullYear()} created by&nbsp;
+      <a href='https://github.com/Xenko10'>Jakub Kołaczyński</a>
+    </footer>
+  );
+}
