e9fe0cb6278fd5520b3a43c95579bd1d17319490
Xenko10 <kubinios_2002@o2.pl>
2024-02-13 10:41:51 +0100

    add hero section, change navbar position

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.description}>
+        <h1 className={styles.title}>Hi, I am Jakub.</h1>
+        <p className={styles.subtitle}>Frontend Developer</p>
+      </div>
+      <img src='./src/assets/hero.png' alt='frontend developer' />
+    </div>
+  );
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,14 +3,16 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <div className={styles.logo}>
-        Jakub Kołaczyński<span className={styles.dot}>.</span>
+      <div className={styles.logoListWrapper}>
+        <div className={styles.logo}>
+          Jakub Kołaczyński<span className={styles.dot}>.</span>
+        </div>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li className={styles.contact}>Contact</li>
+        </ul>
       </div>
-      <ul>
-        <li>About</li>
-        <li>Projects</li>
-        <li className={styles.contact}>Contact</li>
-      </ul>
     </nav>
   );
 }
