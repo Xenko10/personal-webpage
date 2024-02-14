a0ed6eb7ba732365c374b43380b99cf63d1c68f7
Xenko10 <kubinios_2002@o2.pl>
2024-02-14 11:55:32 +0100

    change font and other certain css properties

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,12 +3,14 @@ import styles from "./About.module.css";
 export default function About() {
   return (
     <div id='about' className={styles.about}>
-      <p>
-        I am an ambitious computer science student who always achieves goals
-        without leaving them to the last minute. I compensate for the lack of
-        experience with a positive attitude and a willingness to learn. You can
-        always rely on me.
-      </p>
+      <div className={styles.pWrapper}>
+        <p>
+          I am an ambitious computer science student who always achieves goals
+          without leaving them to the last minute. I compensate for the lack of
+          experience with a positive attitude and a willingness to learn. You
+          can always rely on me.
+        </p>
+      </div>
       <div className={styles.imgWrapper}>
         <img src='./src/assets/about.jpg' alt='photo of me' />
       </div>
