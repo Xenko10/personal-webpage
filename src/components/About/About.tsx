a967ceec986bd99fd2be1e8eefd7af67fa818470
Xenko10 <kubinios_2002@o2.pl>
2024-02-18 14:07:32 +0100

    move h2 closer to p (about section)

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,15 +3,17 @@ import styles from "./About.module.css";
 export default function About() {
   return (
     <div id='about' className={styles.about}>
-      <h2>About</h2>
-      <div className={styles.imgAndpWrapper}>
+      <div className={styles.aboutContent}>
+        <div>
+          <h2>About</h2>
+          <p>
+            I am an ambitious computer science student who always achieves goals
+            without leaving them to the last minute. I compensate for the lack
+            of experience with a positive attitude and a willingness to learn.
+            You can always rely on me.
+          </p>
+        </div>
         <img src='./about.jpg' alt='photo of me' />
-        <p>
-          I am an ambitious computer science student who always achieves goals
-          without leaving them to the last minute. I compensate for the lack of
-          experience with a positive attitude and a willingness to learn. You
-          can always rely on me.
-        </p>
       </div>
     </div>
   );
