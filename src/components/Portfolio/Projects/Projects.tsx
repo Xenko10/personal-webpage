a1a0c4a72970009b178896514c379ccfa7ac1b45
Xenko10 <kubinios_2002@o2.pl>
2024-02-18 08:13:26 +0100

    make project fully responsive

--- src/components/Portfolio/Projects/Projects.tsx
+++ src/components/Portfolio/Projects/Projects.tsx
@@ -6,30 +6,49 @@ export default function Projects() {
       <h2>Projects</h2>
       <div className={styles.projectsWrapper}>
         <div className={styles.project}>
-          <img
-            src='./src/assets/projects/threejs-room.png'
-            alt='Three.js room'
-          />
-          <h3>Three.js room</h3>
-          <p>3D room made in Three.js</p>
+          <a href='https://github.com/Xenko10/threejs-room'>
+            <img
+              src='./src/assets/projects/threejs-room.png'
+              alt='Three.js room'
+            />
+            <div className={styles.projectInfoWrapper}>
+              <h3>Three.js room</h3>
+              <p>3D room made in Three.js</p>
+            </div>
+          </a>
         </div>
         <div className={styles.project}>
-          <img src='./src/assets/projects/flashcards.png' alt='Flashcards' />
-          <h3>Flashcards</h3>
-          <p>Flashcards app using React, Typescript and MySQL</p>
+          <a href='https://github.com/Xenko10/flashcards-app'>
+            <img src='./src/assets/projects/flashcards.png' alt='Flashcards' />
+            <div className={styles.projectInfoWrapper}>
+              <h3>Flashcards</h3>
+              <p>Flashcards app using React, Typescript and MySQL</p>
+            </div>
+          </a>
         </div>
         <div className={styles.project}>
-          <img
-            src='./src/assets/projects/spotify-quiz.png'
-            alt='Spotify quiz'
-          />
-          <h3>Spotify quiz</h3>
-          <p>Your top 50 songs quiz using Spotify api and EJS</p>
+          <a href='https://github.com/Xenko10/spotify-quiz'>
+            <img
+              src='./src/assets/projects/spotify-quiz.png'
+              alt='Spotify quiz'
+            />
+            <div className={styles.projectInfoWrapper}>
+              <h3>Spotify quiz</h3>
+              <p>Your top 50 songs quiz using Spotify api and EJS</p>
+            </div>
+          </a>
         </div>
         <div className={styles.project}>
-          <img src='./src/assets/projects/weather-app.png' alt='Weather app' />
-          <h3>Weather app</h3>
-          <p>Weather app in React and Typescript</p>
+          <a href='https://github.com/Xenko10/weather-app'>
+            <img
+              src='./src/assets/projects/weather-app.png'
+              alt='Weather app'
+            />
+            <div className={styles.projectInfoWrapper}>
+              <h3>Weather app</h3>
+              <p>Weather app in React and Typescript</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
