450074894988f1def3f5022b5c0523b5764c7b79
Xenko10 <kubinios_2002@o2.pl>
2024-02-18 12:55:01 +0100

    change about section positioning and font family

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,16 +3,14 @@ import styles from "./About.module.css";
 export default function About() {
   return (
     <div id='about' className={styles.about}>
-      <div className={styles.pWrapper}>
+      <div className={styles.imgAndpWrapper}>
+        <img src='./about.jpg' alt='photo of me' />
         <p>
           I am an ambitious computer science student who always achieves goals
           without leaving them to the last minute. I compensate for the lack of
           experience with a positive attitude and a willingness to learn. You
           can always rely on me.
         </p>
-      </div>
-      <div className={styles.imgWrapper}>
-        <img src='./about.jpg' alt='photo of me' />
       </div>
     </div>
   );
