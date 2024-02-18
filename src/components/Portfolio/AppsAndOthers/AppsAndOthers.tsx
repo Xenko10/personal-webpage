f04674e7b39c1bf6ca6b23a9e86f3f662f43c6a5
Xenko10 <kubinios_2002@o2.pl>
2024-02-18 12:42:51 +0100

    change positioning of list

--- src/components/Portfolio/AppsAndOthers/AppsAndOthers.tsx
+++ src/components/Portfolio/AppsAndOthers/AppsAndOthers.tsx
@@ -3,24 +3,26 @@ import styles from "./AppsAndOthers.module.css";
 export default function Apps() {
   return (
     <div className={styles.apps}>
-      <h2>Applications & others</h2>
-      <ul>
-        <li>
-          <b>Spotify to Youtube playlist converter</b> (
-          <a href='https://github.com/Xenko10/spotify-to-youtube-playlist-converter'>
-            link
-          </a>
-          )
-        </li>
-        <li>
-          <b>To do list app in React</b> (
-          <a href='https://github.com/Xenko10/todo-app'>link</a>)
-        </li>
-        <li>
-          <b>To do list app in EJS</b> (
-          <a href='https://github.com/Xenko10/todo-list-app'>link</a>)
-        </li>
-      </ul>
+      <div className={styles.h2ulWrapper}>
+        <h2>Applications & others</h2>
+        <ul>
+          <li>
+            <b>Spotify to Youtube playlist converter</b> (
+            <a href='https://github.com/Xenko10/spotify-to-youtube-playlist-converter'>
+              link
+            </a>
+            )
+          </li>
+          <li>
+            <b>To do list app in React</b> (
+            <a href='https://github.com/Xenko10/todo-app'>link</a>)
+          </li>
+          <li>
+            <b>To do list app in EJS</b> (
+            <a href='https://github.com/Xenko10/todo-list-app'>link</a>)
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
